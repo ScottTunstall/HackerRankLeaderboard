f7df2746a24b375b1ec2422ea7be267abeca2b08
Scott Tunstall <Scott.tunstall@ntlworld.com>
2019-06-04 21:02:18 +0100

    Just getting code to build in first place.

--- Leaderboard/Leaderboard/Program.cs
+++ Leaderboard/Leaderboard/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
 
 namespace Leaderboard
 {
     class Program
     {
+        // Complete the climbingLeaderboard function below.
+        static int[] climbingLeaderboard(int[] scores, int[] alice)
+        {
+            //for (int i = 0; i < alice.length; i++)
+            return new int[] { };
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int scoresCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp))
+            ;
+            int aliceCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] alice = Array.ConvertAll(Console.ReadLine().Split(' '), aliceTemp => Convert.ToInt32(aliceTemp))
+            ;
+            int[] result = climbingLeaderboard(scores, alice);
+
+            textWriter.WriteLine(string.Join("\n", result));
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
